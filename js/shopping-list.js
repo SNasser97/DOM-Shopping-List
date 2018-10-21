44bafc2c87beec84ffa3f3ef80f6d3d58bea4ff8
Nasser Ahmed <37514151+SNasser97@users.noreply.github.com>
2018-10-21 16:29:02 +0100

    shopping list script

---
+++ js/shopping-list.js
@@ -0,0 +1,62 @@
+var btn = document.getElementById('enter');
+var input = document.getElementById('userinput');
+var ul = document.querySelector('ul');
+var list = document.querySelectorAll('li');
+var delBtn = document.querySelectorAll('li .delete');
+
+delBtn.forEach(function(e) {
+  e.addEventListener('click', removeListItem);
+})
+
+list.forEach(function(e) {
+  e.addEventListener('click', toggleLi);
+})
+
+function toggleLi() {
+  this.classList.toggle('done'); 
+}
+
+function inputLength() {
+  return input.value.length;
+}
+
+function removeListItem(e) {
+  this.addEventListener('click', function(e) {
+    var item = e.target.parentNode;
+    item.parentNode.removeChild(item);
+  })
+}
+
+function createBtn() {
+  var newDelBtn = document.createElement('button');
+  var btnTxt = document.createTextNode('X');
+  newDelBtn.setAttribute('class', 'delete');
+  newDelBtn.appendChild(btnTxt);
+  return newDelBtn;
+}
+
+function createListElement() {
+    var li = document.createElement('li');
+    li.addEventListener('click', toggleLi); 
+    li.addEventListener('click', removeListItem);
+    li.appendChild(document.createTextNode(input.value));
+    li.appendChild(createBtn()); 
+    ul.appendChild(li); 
+    input.value = ''; 
+}
+
+function addListFromClick() {
+  if(inputLength() > 0) { 
+    createListElement() ;
+  }
+}
+
+function addListFromKeyPress(event) {
+  if(inputLength() > 0 && event.keyCode === 13) { 
+    createListElement()
+  };
+}
+
+btn.addEventListener('click', addListFromClick);
+input.addEventListener('keypress', addListFromKeyPress);
+
